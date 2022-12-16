82b5644e19eac7236fe4a8a9c33b3bf2b0a9f7b2
Nur Farah <nuurfaarax15@gmail.com>
2022-12-16 19:18:28 +0300

    repo name changed

---
+++ Lectures/Methods.java
@@ -0,0 +1,19 @@
+package Tests;
+
+public class Methods {
+    public static void main(String[] args) {
+//        myfunction("Nur Farah",20);
+
+        int z = intfunction(9,12);
+        System.out.println(z);
+    }
+//    static void myfunction(String myname,int age){
+////        System.out.println("I am studying Java Methods");
+//        System.out.println(myname + " is my name and i am " + age + " years old");
+//    }
+
+    static int intfunction(int x,int y){
+        return x - y;
+
+    }
+}
