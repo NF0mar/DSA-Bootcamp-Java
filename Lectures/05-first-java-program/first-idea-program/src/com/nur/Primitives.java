59b40f2b4aa03c3a2f83c395bfb62397542f1780
Nur-Farah <nuurfaarax15@gmail.com>
2023-12-11 06:18:03 +0300

    learnt java primitive datatypes

---
+++ Lectures/05-first-java-program/first-idea-program/src/com/nur/Primitives.java
@@ -0,0 +1,22 @@
+package com.nur;
+
+import java.util.Scanner;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        int i = 90;
+        char c = 'N';
+        float f = 9403.33f;
+        long l = 9258432041698749L;
+        double d = 330838949.239394;
+        boolean b = false;
+        // holds value from -128 to 127
+        byte mini = -128;
+        byte maxi = 127;
+
+        //holds value from -32768 to 32767
+        short min = -32768;
+        short max = 32767;
+    }
+}
